dd47c8a2fc119071715aa0757fa5c48cf0c3295c
Noelzy1 <100541467+Noelzy1@users.noreply.github.com>
2023-02-16 09:40:25 +1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Noel Shedd on 16/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
